5b90b27df92afc84fc0dbf7f088a457d4a8d3e3a
Brendan Bosman <bhbosman@gmail.com>
2020-09-17 14:35:01 +0200

    WIP

---
+++ item.go
@@ -0,0 +1,28 @@
+package gorxextra
+
+import (
+	"context"
+	rxgo "github.com/ReactiveX/RxGo"
+	"github.com/bhbosman/gocommon/constants"
+	"time"
+)
+
+func SendContextWithTimeOutAndRetries(i rxgo.Item, ctx context.Context, duration time.Duration, retryCount int, ch chan rxgo.Item) error {
+	newTimer := time.NewTimer(duration)
+	defer newTimer.Stop()
+	for r := 1; r <= retryCount; r++ {
+		newTimer.Reset(duration)
+		select {
+		case <-newTimer.C:
+			if r == retryCount {
+				return constants.TimeOut
+			}
+			continue
+		case <-ctx.Done():
+			return ctx.Err()
+		case ch <- i:
+			return nil
+		}
+	}
+	return constants.InvalidParam
+}
