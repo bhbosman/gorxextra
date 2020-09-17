dbb7b4338773dfcec4c09b3384a41ef73b18bdb0
Brendan Bosman <bhbosman@gmail.com>
2020-09-17 14:36:10 +0200

    WIP

--- item.go
+++ item.go
@@ -7,7 +7,13 @@ import (
 	"time"
 )
 
-func SendContextWithTimeOutAndRetries(i rxgo.Item, ctx context.Context, duration time.Duration, retryCount int, ch chan rxgo.Item) error {
+func SendContextWithTimeOutAndRetries(
+	i rxgo.Item,
+	ctx context.Context,
+	duration time.Duration,
+	retryCount int,
+	ch chan rxgo.Item) error {
+
 	newTimer := time.NewTimer(duration)
 	defer newTimer.Stop()
 	for r := 1; r <= retryCount; r++ {
